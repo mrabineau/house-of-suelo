09e348ea9d70eda100512fded5c9a25e77f4577d
Mel Rabineau <melanie.rabineau1@gmail.com>
2016-12-14 17:47:00 -0800

    buttons strobbing

--- src/components/About.js
+++ src/components/About.js
@@ -36,19 +36,8 @@ class About extends Component {
     super(props);
     this.state = {images: ["jacket.JPG", "collar.JPG", "boobs2.JPG", "crabhat.JPG", "fries.JPG", "peli.JPG", "hat.JPG", "jacket1.JPG", "naked.JPG", "pasta.JPG", "soda.JPG"],
     selected:0}
-    // this.state = {colors: ['#F7CA18', '#26A65B', '#003171', '#BF55EC', '#F62459', '#DC3023'], selected: 0}
     this.handleAdd = this.handleAdd.bind(this);
-    // this.colorChange = this.colorChange.bind(this);
   }
-
-   // colorChange () {
-   //    const nextColor = this.state.selected +1;
-   //     if (nextColor >= this.state.colors.length){
-   //       this.setState({selected: 0})
-   //     } else {
-   //       this.setState({selected: nextColor})
-   //     }
-   //   }
 
 
   handleAdd () {
@@ -68,7 +57,7 @@ class About extends Component {
         const rootURL = `/House_of_Suelo_pix/${this.state.images[selected]}`;
         return (
             <div className="About">
-                      <button type="button" className="topbtn"><Link to="/info"> BABY CONSUELO </Link></button>
+                      <button id="top" type="button" className="btn"><Link to="/info"> WHO EVEN <strong> IS </strong> BBY CONSUELO </Link></button>
                <div  className="main">
                   <h1 id="blue"> CUSTOMIZE YOUR SHIT </h1>
 
@@ -79,7 +68,7 @@ class About extends Component {
                             <img id="addImage" src={rootURL} alt="img"></img>
                         </ReactCSSTransitionGroup>
                   </div>
-               <button type="button" className="btn" onClick={this.handleAdd}>TAKE A LOOK</button>
+               <button id="bottom" type="button" className="btn" onClick={this.handleAdd}>TAKE A LOOK</button>
             </div>
 
         );
